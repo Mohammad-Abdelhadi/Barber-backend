f7c6cacb76130d8530f99cd15c3cf7e0426b098e
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-09-11 18:21:47 +0300

    create the backend of the project

---
+++ backend/controllers/userController.js
@@ -0,0 +1,66 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+// login a user
+const loginUser = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  try {
+    const user = await User.login(email, password, role);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token, role: user.role });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// signup a user
+const signupUser = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  try {
+    const user = await User.signup(email, password, role);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token, role });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+// Get All users
+const getAllusers = async (req, res) => {
+  const allusers = await User.find({});
+
+  try {
+    res.status(200).json(allusers);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const deleteUser = async (req, res) => {
+  const { id } = req.params; // Use req.params.id
+  try {
+    const deletedUser = await User.findOneAndDelete({ _id: id });
+
+    if (deletedUser) {
+      res
+        .status(200)
+        .json({ message: "User deleted successfully", deletedUser });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { signupUser, loginUser, getAllusers, deleteUser };
---
+++ backend/routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+// controller functions
+const {
+  loginUser,
+  signupUser,
+  getAllusers,
+  deleteUser,
+} = require("../controllers/userController");
+
+const router = express.Router();
+
+// Get all users
+router.get("/", getAllusers);
+// Get all users
+router.delete("/delete/:id", deleteUser);
+// login route
+router.post("/login", loginUser);
+
+// signup route
+router.post("/signup", signupUser);
+
+module.exports = router;
