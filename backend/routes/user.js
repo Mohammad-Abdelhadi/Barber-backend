6c1ecf8f85df55e46675f7eb0fc2836302d12c3c
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-09-14 06:49:06 +0300

    done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -17,7 +17,7 @@ const {
 const router = express.Router();
 
 // Get all users
-router.get("/", AdminRequireAuth, getAllusers);
+router.get("/", getAllusers);
 // Get all users
 router.delete("/delete/:id", deleteUser);
 // login route
